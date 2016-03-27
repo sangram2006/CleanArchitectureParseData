811663124f4255e1c8d76e577f59900e82778092
Sangram Mohanty <sangram.mohanty3@gmail.com>
2016-03-27 16:41:00 +0530

    Implemented Parcelable for server response

--- ParseDataApplication/Parse-Data/src/main/java/mobile/saltside/com/parsedataapplication/DetailActivity.java
+++ ParseDataApplication/Parse-Data/src/main/java/mobile/saltside/com/parsedataapplication/DetailActivity.java
@@ -3,21 +3,28 @@ package mobile.saltside.com.parsedataapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import mobile.handler.com.handler_lib.ServeResponseMessage;
+
 /**
  * Created by sangram.
  */
 public class DetailActivity extends AppCompatActivity {
+    public static final String SAVE_DATA = "SAVE_DATA";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
+        Bundle b = getIntent().getExtras();
         if (savedInstanceState == null) {
-            DetailFragment fragment = new DetailFragment();
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.detail_activity, fragment, DetailFragment.FRAGMENT_TAG)
-                    .disallowAddToBackStack()
-                    .commit();
+            if (b != null) {
+                DetailFragment fragment = DetailFragment.newInstance((ServeResponseMessage) b.getParcelable(SAVE_DATA));
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.detail_activity, fragment, DetailFragment.FRAGMENT_TAG)
+                        .disallowAddToBackStack()
+                        .commit();
+            }
         }
     }
 }
